d10f97b7bbc747e26b506a28864f56f94f66cfe1
Marco <maurelima@gmail.com>
2021-08-17 20:13:02 -0300

    Cleaning the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 
-import Header from '../components/Header'
-
 import { Container } from '../styles/pages/home'
 
 const Home: React.FC = () => {
     return (
         <Container>
             <Head>
-                <title>SteamBackground</title>
+                <title>Boilerplate</title>
             </Head>
-            <Header />
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 md:px-8 mt-20">
-                <div className="flex justify-center items-center ">
-                    <div className="w-4/5 lg:w-2/4 text-center">
-                        <h1 className="leading-none tracking-tight">
-                            Hello World
-                        </h1>
-                        <p>Boilerplate Next.Js</p>
-                    </div>
-                </div>
-            </div>
         </Container>
     )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,15 +2,4 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     width: 100%;
-    background-image: linear-gradient(#092a43, #040305);
-    h1 {
-        font-size: 20px;
-        font-weight: bold;
-        font-family: 'Roboto', sans-serif;
-    }
-    h2 {
-        font-size: 20px;
-        font-weight: bolder;
-        font-family: 'Inter', sans-serif;
-    }
 `
